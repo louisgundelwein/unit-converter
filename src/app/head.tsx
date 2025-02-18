52615bb042e0e91f98d3e0b5f382bfc4d50c8729
Louis Gundelwein <louis-gundelwein@gmx.de>
2025-02-18 22:22:10 +0100

    feat: google ads script

---
+++ src/app/head.tsx
@@ -0,0 +1,15 @@
+// app/head.tsx
+export default function Head() {
+	return (
+		<>
+			<title>Deine Website</title>
+			<meta name="description" content="Toolbox for different useful purposes" />
+			{/* Google AdSense Script */}
+			<script
+				async
+				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6105108199502947"
+				crossOrigin="anonymous"
+			></script>
+		</>
+	);
+}
